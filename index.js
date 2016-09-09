7214352e63757b3f8c4c58f4851d329c02b0cee6
s tech <email@sarcas.tech>
2016-09-08 22:33:50 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 'use strict'
 
-function voorhees (req, res, next) {
-  res.voorhees = function (data, view) {
-    req.xhr ? res.json(data) : res.render(view, data || {})
+function Voorhees (req, res) {
+  this.view = ''
+  this.req = req
+  this.res = res
+}
+
+Voorhees.prototype = {
+  setView: (view) => {
+    this.view = view
+    return this
+  },
+  respond: (data) => {
+    let res = this.res
+    this.req.xhr ? res.json(data) : res.render(this.view, data || {})
   }
+}
+
+function middleware (req, res, next) {
+  res.voorhees = new Voorhees(req, res)
   next()
 }
 
-module.exports = voorhees
+module.exports = middleware
